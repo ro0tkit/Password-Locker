e4236ecdf1d30e498883e0bdf734342503779060
Luther <lutherking30@gmail.com>
2021-10-26 14:50:12 +0300

    Added unnittest

--- test_user_credentials.py
+++ test_user_credentials.py
@@ -6,8 +6,6 @@ class TestUser(unittest.TestCase):
     '''
     Test class that defines test cases for the User class behaviours.
 
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
     '''
     def setUp(self):
         '''
